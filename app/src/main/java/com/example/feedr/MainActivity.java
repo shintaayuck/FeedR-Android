4b78642f80121e3793d9bd269d1740ce62c85ee3
alifalfa73 <alifalfarisy73@gmail.com>
2019-02-21 20:21:37 +0700

    Add Notification Requirement

--- app/src/main/java/com/example/feedr/MainActivity.java
+++ app/src/main/java/com/example/feedr/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.feedr;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  String TAG = "INI TAG BANGSAT";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +51,6 @@ public class MainActivity extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
+
     }
 }
